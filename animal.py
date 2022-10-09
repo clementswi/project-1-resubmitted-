e9f208a967c13bdb1bbf139d48b43d1b00120d4c
clementswi <perryclements@gmail.com>
2022-10-09 11:02:41 -0700

    Project 1 Resubmission, Had to create new github repository in order to commit and push

---
+++ animal.py
@@ -0,0 +1,5 @@
+#Author: William Clements
+#Github username: clemenwi
+#Description: Asks the user for their favorite animal
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the "+ fave_animal + ".")
